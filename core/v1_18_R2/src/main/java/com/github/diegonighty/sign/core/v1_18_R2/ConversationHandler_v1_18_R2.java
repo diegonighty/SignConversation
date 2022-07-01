3afe055259c1fdb1105b66be369f6a6dbec8a967
Diego Cardenas <49890496+DiegoNighty@users.noreply.github.com>
2022-07-01 15:52:05 -0500

    feat: added support for 1.18.2 and 1.19!

---
+++ core/v1_18_R2/src/main/java/com/github/diegonighty/sign/core/v1_18_R2/ConversationHandler_v1_18_R2.java
@@ -0,0 +1,41 @@
+package com.github.diegonighty.sign.core.v1_18_R2;
+
+import com.github.diegonighty.sign.core.AbstractSignService;
+import com.github.diegonighty.sign.core.packet.PacketChannelDuplexHandler;
+import net.minecraft.core.BlockPosition;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
+import net.minecraft.network.protocol.game.PacketPlayOutOpenSignEditor;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class ConversationHandler_v1_18_R2 extends AbstractSignService {
+
+	@Override
+	protected void addInterceptors() {
+		PacketChannelDuplexHandler.addInterceptor(PacketPlayInUpdateSign.class, new SignListener(this));
+	}
+
+	@Override
+	public void openFakeSign(Player player, Location location) {
+		BlockPosition position = new BlockPosition(
+				location.getBlockX(),
+				location.getBlockY(),
+				location.getBlockZ()
+		);
+
+		Packet<?> packet = new PacketPlayOutOpenSignEditor(position);
+		((CraftPlayer) player)
+				.getHandle()
+				.b
+				.a(packet);
+	}
+
+	@Override
+	public void injectPlayer(Player player) {
+		((CraftPlayer) player).getHandle()
+				.b.a.m.pipeline()
+				.addBefore("packet_handler", CHANNEL, new PacketChannelDuplexHandler(player));
+	}
+}
---
+++ core/v1_19_R1/src/main/java/com/github/diegonighty/sign/core/v1_19_R1/SignListener.java
@@ -0,0 +1,33 @@
+package com.github.diegonighty.sign.core.v1_19_R1;
+
+import com.github.diegonighty.sign.api.conversation.SignConversation;
+import com.github.diegonighty.sign.core.AbstractSignService;
+import com.github.diegonighty.sign.core.BukkitExecutor;
+import com.github.diegonighty.sign.core.packet.PacketInterceptor;
+import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
+import org.bukkit.entity.Player;
+
+public class SignListener implements PacketInterceptor<PacketPlayInUpdateSign> {
+
+	private final AbstractSignService signService;
+
+	public SignListener(AbstractSignService signService) {
+		this.signService = signService;
+	}
+
+	@Override
+	public PacketPlayInUpdateSign in(Player player, PacketPlayInUpdateSign packet) {
+		SignConversation conversation = signService.getAndRemove(player);
+
+		if (conversation != null) {
+			BukkitExecutor.BukkitExecutorProvider.get()
+					.execute(() -> {
+						conversation.callback()
+								.then(player, packet.c())
+								.response(player);
+					});
+		}
+
+		return packet;
+	}
+}
